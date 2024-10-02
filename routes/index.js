d2411969329970db049e9c9afcb846eaf24c95cc
Gabriel <gabriel.schwartzmann@free.fr>
2024-10-02 15:05:32 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+const fetch = require('node-fetch');
+var router = express.Router();
+
+const apiKey = process.env.TMDB_API_KEY;
+
+router.get('/movies', (req, res) => {
+
+			fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`)
+				.then(response => response.json())
+				.then(apiData => {
+					console.log(apiData);
+					res.json({ movies: apiData.results})
+                })
+            });
+
+module.exports = router;
